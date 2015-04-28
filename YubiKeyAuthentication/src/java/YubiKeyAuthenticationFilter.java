e75f3a4de0f72e36d4f3b334a9d03b09b7742583
ikramulhaq <ikramulhaq23@yahoo.com>
2015-04-28 12:52:47 +0500

    Changed authentication flow.

--- YubiKeyAuthentication/src/java/YubiKeyAuthenticationFilter.java
+++ YubiKeyAuthentication/src/java/YubiKeyAuthenticationFilter.java
@@ -5,20 +5,15 @@ import com.yubico.client.v2.exceptions.YubicoValidationException;
 import com.yubico.client.v2.exceptions.YubicoValidationFailure;
 import org.codehaus.groovy.grails.plugins.springsecurity.SecurityRequestHolder;
 import org.springframework.security.authentication.AuthenticationServiceException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-import org.springframework.security.web.util.TextEscapeUtils;
-import org.springframework.util.Assert;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import javax.security.sasl.AuthenticationException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import static com.yubico.client.v2.YubicoClient.isValidOTPFormat;
@@ -26,63 +21,31 @@ import static com.yubico.client.v2.YubicoClient.isValidOTPFormat;
 /**
  *  processes an authentication form submission
  *  require Three parameters UserName ,Password and YubiKeyOTP
- * author: Ikram
+ *  author: Ikram
  * Date: 12/5/14
  * Time: 12:49 PM
  *
  */
 
-public class YubiKeyAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
-    public static final String SPRING_SECURITY_FORM_USERNAME_KEY = "j_username";
-    public static final String SPRING_SECURITY_FORM_PASSWORD_KEY = "j_password";
-    public static final String SPRING_SECURITY_FORM_YUBIKEYOTP_KEY = "j_OTP";
-    public static final String SPRING_SECURITY_LAST_USERNAME_KEY = "SPRING_SECURITY_LAST_USERNAME";
-
-    private String usernameParameter = SPRING_SECURITY_FORM_USERNAME_KEY;
-    private String passwordParameter = SPRING_SECURITY_FORM_PASSWORD_KEY;
-    private String yubiKeyOTPParameter = SPRING_SECURITY_FORM_YUBIKEYOTP_KEY;
-    private boolean postOnly = true;
-
+public class YubiKeyAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     public YubiKeyAuthenticationFilter() {
-        super("/j_spring_security_check");
+        super();
     }
 
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        if (postOnly && !request.getMethod().equals("POST")) {
-            throw new AuthenticationServiceException("Authentication method not supported: " + request.getMethod());
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response){
+
+        String yubiKeyOneTimePassword = request.getParameter("j_oneTimePassword");
+
+        if (yubiKeyOneTimePassword == null) {
+            yubiKeyOneTimePassword = "";
         }
-
-        String username = obtainUsername(request);
-        String password = obtainPassword(request);
-        String yubiKeyOTP = obtainYubiKeyOTP(request);
-
-        if (username == null) {
-            username = "";
-        }
-        username = username.trim();
-
-        if (password == null) {
-            password = "";
-        }
-
-        if (yubiKeyOTP == null) {
-            yubiKeyOTP = "";
-        }
-
-        UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(username, password);
-
-        HttpSession session = request.getSession(false);
-        if (session != null || getAllowSessionCreation()) {
-            request.getSession().setAttribute(SPRING_SECURITY_LAST_USERNAME_KEY, TextEscapeUtils.escapeEntities(username));
-        }
-
         String yubiKeyPublicId ;
         YubicoClient client = YubicoClient.getClient(1);
         YubicoResponse responseOTP = null;
         try {
-             if (isValidOTPFormat(yubiKeyOTP))
-                responseOTP = client.verify(yubiKeyOTP);
+             if (isValidOTPFormat(yubiKeyOneTimePassword))
+                responseOTP = client.verify(yubiKeyOneTimePassword);
              else  {
                  logger.info("Authentication Failed");
                 throw new  AuthenticationServiceException("Login Fail");
@@ -95,52 +58,15 @@ public class YubiKeyAuthenticationFilter extends AbstractAuthenticationProcessin
             throw new  AuthenticationServiceException("Login Fail");
         }
         if (responseOTP.getStatus() == YubicoResponseStatus.OK) {
-             yubiKeyPublicId = YubicoClient.getPublicId(yubiKeyOTP);
+             yubiKeyPublicId = YubicoClient.getPublicId(yubiKeyOneTimePassword);
              logger.info("Authentication Successful");
         }
         else  {
             logger.info("Authentication Failed");
             throw new  AuthenticationServiceException("Login Fail");
         }
-        setDetails(request, authRequest);
-        return this.getAuthenticationManager().authenticate(authRequest);
 
-    }
-
-
-    protected String obtainPassword(HttpServletRequest request) {
-        return request.getParameter(passwordParameter);
-    }
-
-    protected String obtainUsername(HttpServletRequest request) {
-        return request.getParameter(usernameParameter);
-    }
-
-    protected String obtainYubiKeyOTP(HttpServletRequest request){
-        return request.getParameter(yubiKeyOTPParameter);
-    }
-
-    protected void setDetails(HttpServletRequest request, UsernamePasswordAuthenticationToken authRequest) {
-        authRequest.setDetails(authenticationDetailsSource.buildDetails(request));
-    }
-
-    public void setUsernameParameter(String usernameParameter) {
-        Assert.hasText(usernameParameter, "Username parameter must not be empty or null");
-        this.usernameParameter = usernameParameter;
-    }
-
-    public void setPasswordParameter(String passwordParameter) {
-        Assert.hasText(passwordParameter, "Password parameter must not be empty or null");
-        this.passwordParameter = passwordParameter;
-    }
-
-    public void setPostOnly(boolean postOnly) {
-        this.postOnly = postOnly;
-    }
-
-
-    public final String getPasswordParameter() {
-        return passwordParameter;
+        return super.attemptAuthentication(request, response);
     }
 
     @Override
