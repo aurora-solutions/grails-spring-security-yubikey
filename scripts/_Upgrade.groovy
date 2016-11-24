2e07717b301468379e266704749a163af4a0c88a
ahmad <ahmad.iqbal@jbilling.com>
2016-11-24 11:53:56 +0500

    Add license

--- scripts/_Upgrade.groovy
+++ scripts/_Upgrade.groovy
@@ -1,3 +1,33 @@
+/***************************************************************************** 
+* Copyright 2016 Aurora Solutions 
+* 
+*    http://www.aurorasolutions.io 
+* 
+* Aurora Solutions is an innovative services and product company at 
+* the forefront of the software industry, with processes and practices 
+* involving Domain Driven Design(DDD), Agile methodologies to build 
+* scalable, secure, reliable and high performance products.
+* 
+* The Grails YubiKey Authentication plugin adds authentication support for
+* YubiKey, which is a is a professional Two-Factor Authentication (2FA)
+* solution, to a Grails application that uses Spring Security filters.
+* The plugin depends on the Spring Security Core plugin. The plugin
+* authenticates YubiKey OTP (One-Time Password) using yubico-java-client.
+* 
+* Licensed under the Apache License, Version 2.0 (the "License"); 
+* you may not use this file except in compliance with the License. 
+* You may obtain a copy of the License at 
+* 
+*    http://www.apache.org/licenses/LICENSE-2.0 
+* 
+* Unless required by applicable law or agreed to in writing, software 
+* distributed under the License is distributed on an "AS IS" BASIS, 
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+* See the License for the specific language governing permissions and 
+* limitations under the License. 
+*****************************************************************************/
+
+
 //
 // This script is executed by Grails during application upgrade ('grails upgrade'
 // command). This script is a Gant script so you can use all special variables
